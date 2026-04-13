033450ba98d232eef61ec5501fef071f5c644828
Zoe-Frost <zoe.frost@mds.ac.nz>
2026-04-13 14:01:16 +1200

    Drawing everything

--- script.js
+++ script.js
@@ -30,5 +30,34 @@ async function setupCamera(params) {
 
 // The segmentation setup
 function setupSegmentation() {
-    
+    segmentation = new SelfieSegmentation({
+        locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}` // this is the file for the segmentation software//
+
+    });
+
+    segmentation.setOptions({
+        modelSelection: 1
+    });
+
+    segmentation.onResults(onResults);
+}
+
+//Drawing everything - the fullscreen, mirror, and texture
+function onResults(results) {
+
+    const cw = canvas.width;
+    const ch = canvas.height;
+
+    const vw = results.image.width;
+    const vh = results.image.height;
+
+    //the scale to cover the screen
+    const scale = Math.max(cw/vw, ch,vh);
+
+    const drawWidth = vw * scale;
+    const drawHeight = vh * scale;
+
+    const offsetX = (cw - drawWidth) / 2;
+    const offsetY = ()
+
 }
