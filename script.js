ad96252351ba02506a85382756a25b27145f483d
Zoe-Frost <zoe.frost@mds.ac.nz>
2026-04-13 12:40:35 +1200

    camera setup

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const texture = document.getElementById("texture");
 const ctx = canvas.getContext("2d");
+
+let segmentation;
+
+// Resising the canvas to fit the full screen
+function resizeCanvas() {
+    canvas.Width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+// The camera set up
+async function setupCamera(params) {
+    const stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 640, height: 480}
+    });
+
+    video.srcObject = stream;
+
+    return new Promise((resolve) => {
+        video
+    })
+    
+}
