88f696692998ffab589b01fb6ae4e0cb9716a86f
Zoe-Frost <zoe.frost@mds.ac.nz>
2026-04-13 12:34:39 +1200

    js start

---
+++ script.js
@@ -0,0 +1,4 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const texture = document.getElementById("texture");
+const ctx = canvas.getContext("2d");
